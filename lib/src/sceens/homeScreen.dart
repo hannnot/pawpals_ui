8e24a88180072a8881280c09c439012192ae272c
h.tonn <hanna.tonn@edu.tbz.ch>
2020-09-11 09:17:51 +0200

    Login Screen(homeScreen) fixes

--- lib/src/sceens/homeScreen.dart
+++ lib/src/sceens/homeScreen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,9 +9,30 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String email;
+  String pwd;
+
+  final _form = GlobalKey<FormState>();
+
+  void _saveForm() async {
+    if (_form.currentState.validate()) {
+      _form.currentState.save();
+      //TODO make network request to validate input WITH VALID url
+      http.Response response = await http.post('', body: {
+        'email' : email,
+        'pwd': pwd
+      });
+      if(response.statusCode == HttpStatus.accepted){
+        //TODO create dashboard screen
+        Navigator.of(context).pushReplacementNamed('/dashboard');
+      }
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
+    return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
       ),
@@ -18,6 +42,7 @@ class _HomeScreenState extends State<HomeScreen> {
           children: [
             SizedBox(height: 25),
             Form(
+              key: _form,
               child: Column(
                 children: [
                   TextFormField(
@@ -25,14 +50,49 @@ class _HomeScreenState extends State<HomeScreen> {
                     decoration: InputDecoration(
                       labelText: 'Email',
                     ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter email';
+                      }else {
+                        setState(() {
+                          email = value;
+                        });
+                        return null;
+                      }
+                    },
                   ),
+                  SizedBox(height: 10),
                   TextFormField(
                     obscureText: true,
                     decoration: InputDecoration(
                       labelText: 'Password',
                     ),
-                  )
+                     validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter password';
+                      }else {
+                        setState(() {
+                          pwd = value;
+                        });
+                        return null;
+                      }
+                    }
+                  ),
+                  SizedBox(height: 10),
+                  FlatButton(
+                    onPressed: _saveForm, //TODO make request to API to proccede
+                    child: Container(
+                      child: Text('Login'),
+                    ),
+                  ),
                 ],
+              ),
+            ),
+            SizedBox(height: 30),
+            FlatButton(
+              onPressed: null, //TODO Navigate to registration screen
+              child: Container(
+                child: Text('Or register Now'),
               ),
             )
           ],
