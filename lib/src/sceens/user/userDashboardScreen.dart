db41111c54e60f4e5d07b5e99901f5a0902f6272
h.tonn <hanna.tonn@edu.tbz.ch>
2020-09-18 11:16:10 +0200

    userDashboardScreen fixes

--- lib/src/sceens/user/userDashboardScreen.dart
+++ lib/src/sceens/user/userDashboardScreen.dart
@@ -4,8 +4,9 @@ import 'package:pawpals_ui/src/widgets/userDrawer.dart';
 class UserDashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var arguments = ModalRoute.of(context).settings.arguments as Map<String, dynamic>;
     return Scaffold(
-      drawer: UserDrawer(token: null),
+      drawer: UserDrawer(token: arguments['auth']),
       appBar: AppBar(
         title: Text('Welcome'),
       ),
