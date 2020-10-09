c8960e32edf8fe9526bde6cd880dec7afb1c69a0
Hanna Tonn <h.tonn@rafisa.ch>
2020-10-09 12:34:27 +0200

    added splash screen

---
+++ android/app/src/main/java/com/example/pawpals_ui/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.pawpals_ui;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.SplashScreen;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.dart.DartExecutor;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.view.FlutterMain;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends FlutterActivity {
+
+}
+
--- android/app/src/main/kotlin/com/example/pawpals_ui/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pawpals_ui
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
